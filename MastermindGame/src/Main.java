67c7d697fd9d594f36859dbeea33920ad9f812e6
Nicklas W <Nicklas2002@hotmail.com>
2023-08-23 16:29:04 +0200

    TextUI Update
    - Added getIntInput that takes user input and checks if its a valid number.
    
    -Added getStringInput that takes the user input and checks if its one of the valid string options.

--- MastermindGame/src/Main.java
+++ MastermindGame/src/Main.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
     public static void main(String[] args){
         Game game = new Game();
+        TextUI ui = new TextUI();
         // Tænker det her skal over i TextUI.
         // hvis vi vil have at folk kan ændre på hvor mange farver de vil gætte
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hej, velkommen til Mastermind!");
-        System.out.println("how long do you want the color sequence to be?");
+        ui.displayMsg("Hej, velkommen til Mastermind!");
         ////////////////////
         // i tilfælde af vi beholder ideen om at vi kan ændre på længden, skal vi have et tjek på at det faktisk er et tal der bliver sat ind.
         // og det sikre os det holder sig inden for et bestemt interval.
-        int length = scan.nextInt();
-        scan.next();
-
+        int length = ui.getIntInput("how long do you want the color sequence to be?");
+        String test = ui.getStringInput("Test string input");
+        ui.displayMsg(test);
         game.gameStart(length);
 
     }
--- MastermindGame/src/TextUI.java
+++ MastermindGame/src/TextUI.java
@@ -1,7 +1,49 @@
+import java.util.Scanner;
+
 public class TextUI {
+
+    private Scanner scan = new Scanner(System.in);
 
     public void displayMsg(String msg){
         System.out.println(msg);
+    }
+
+
+    public int getIntInput(String msg){
+        displayMsg(msg);
+        boolean validInput = false;
+        int out = 0;
+        while(!validInput){
+            try{
+                out = scan.nextInt();
+                scan.nextLine();
+                validInput = true;
+            }catch(Exception e){
+                displayMsg("Invalid input! Try again.");
+                scan.next();
+            }
+        }
+        return out;
+    }
+
+    public String getStringInput(String msg) {
+        displayMsg(msg);
+        boolean validInput = false;
+        String out = "";
+        while(!validInput){
+            try{
+                out = scan.nextLine().toUpperCase();
+                if(!out.isBlank() || !out.isEmpty()){
+                    if(out.equals("RED") || out.equals("BLUE") || out.equals("YELLOW") || out.equals("GREEN") || out.equals("BLACK")){
+                        validInput = true;
+                    }
+                    displayMsg("Invalid input! Try again!");
+                }
+            }catch(Exception e){
+                displayMsg("Invalid input! Try again!");
+            }
+        }
+        return out;
     }
 
 }
